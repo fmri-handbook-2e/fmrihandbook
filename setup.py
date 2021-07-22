72b9c1a666ec56da982145a0da954d7c9dd9fe5d
Russell Poldrack <poldrack@gmail.com>
2021-07-22 11:10:53 -0700

    remove tests module

--- setup.py
+++ setup.py
@@ -4,28 +4,38 @@
 # some portions borrowed from https://github.com/mwaskom/lyman/blob/master/setup.py
 
 
-descr = """fmrihandbook: Python package with code used to generate figures and examples from Poldrack et al. Handbook of fMRI Data Analysis"""
 
 import os
 from setuptools import setup
 
-DISTNAME="fmrihandbook"
-DESCRIPTION=descr
-MAINTAINER='Russ Poldrack'
-MAINTAINER_EMAIL='poldrack@stanford.edu'
-LICENSE='MIT'
-URL='http://www.fmri-data-analysis.org/'
-DOWNLOAD_URL='https://github.com/fmri-handbook-2e/fmrihandbook'
-VERSION='2021.1'
+descr = """fmrihandbook: Python package with code used to generate figures and examples from Poldrack et al. Handbook of fMRI Data Analysis"""
+DISTNAME = "fmrihandbook"
+DESCRIPTION = descr
+MAINTAINER = "Russ Poldrack"
+MAINTAINER_EMAIL = "poldrack@stanford.edu"
+LICENSE = "MIT"
+URL = "http://www.fmri-data-analysis.org/"
+DOWNLOAD_URL = "https://github.com/fmri-handbook-2e/fmrihandbook"
+VERSION = "2021.1"
+
 
 def check_dependencies():
 
     # Just make sure dependencies exist, I haven't rigorously
     # tested what the minimal versions that will work are
-    needed_deps = ["IPython", "numpy", "scipy", "matplotlib",
-                   "sklearn", "statsmodels", "networkx",
-                   "pandas","nilearn","requests",
-		                 "nibabel"]
+    needed_deps = [
+        "IPython",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "sklearn",
+        "statsmodels",
+        "networkx",
+        "pandas",
+        "nilearn",
+        "requests",
+        "nibabel",
+    ]
     missing_deps = []
     for dep in needed_deps:
         try:
@@ -34,24 +44,23 @@ def check_dependencies():
             missing_deps.append(dep)
 
     if missing_deps:
-        missing = (", ".join(missing_deps)
-                   .replace("sklearn", "scikit-learn"))
+        missing = ", ".join(missing_deps).replace("sklearn", "scikit-learn")
         raise ImportError("Missing dependencies: %s" % missing)
+
 
 if __name__ == "__main__":
 
-    if os.path.exists('MANIFEST'):
-        os.remove('MANIFEST')
+    if os.path.exists("MANIFEST"):
+        os.remove("MANIFEST")
 
     import sys
-    if not (len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
-            sys.argv[1] in ('--help-commands',
-                            '--version',
-                            'egg_info',
-                            'clean'))):
+
+    if not (len(sys.argv) >= 2 and (
+            "--help" in sys.argv[1:] or sys.argv[1] in ("--help-commands", "--version", "egg_info", "clean"))):
         check_dependencies()
 
-    setup(name=DISTNAME,
+    setup(
+        name=DISTNAME,
         maintainer=MAINTAINER,
         maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
@@ -59,17 +68,17 @@ if __name__ == "__main__":
         version=VERSION,
         url=URL,
         download_url=DOWNLOAD_URL,
-        packages=['fmrihandbook', 'fmrihandbook.tests',
-                  'fmrihandbook.utils'],
-#        scripts=['scripts/run_fmri.py', 'scripts/run_group.py',
-#                 'scripts/run_warp.py', 'scripts/setup_project.py',
-#                 'scripts/make_masks.py', 'scripts/anatomy_snapshots.py',
-#                 'scripts/surface_snapshots.py'],
+        packages=["fmrihandbook", "fmrihandbook.utils"],  # 'fmrihandbook.tests',
+        #        scripts=['scripts/run_fmri.py', 'scripts/run_group.py',
+        #                 'scripts/run_warp.py', 'scripts/setup_project.py',
+        #                 'scripts/make_masks.py', 'scripts/anatomy_snapshots.py',
+        #                 'scripts/surface_snapshots.py'],
         classifiers=[
-                     'Intended Audience :: Science/Research',
-                     'Programming Language :: Python :: 3.8',
-                     'License :: OSI Approved :: BSD License',
-                     'Operating System :: POSIX',
-                     'Operating System :: Unix',
-                     'Operating System :: MacOS'],
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+        ],
     )
